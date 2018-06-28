46c43d0301e28687b7dd05caf8d4091c2be147e5
DevinPierce <devinmpierce@gmail.com>
2018-06-28 14:37:13 -0400

    created slug methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,10 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    special_characters_removed = self.name.gsub(/[$&+,:;=?@#|'<>.^*()%!]/, '')
+    special_characters_removed.gsub(/[\s]/, '-').downcase
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    special_characters_removed = self.name.gsub(/[$&+,:;=?@#|'<>.^*()%!]/, '')
+    special_characters_removed.gsub(/[\s]/, '-').downcase
+  end
+
 end
