a9500811b14690ab698c80169a9f29e66b35e8c8
DevinPierce <devinmpierce@gmail.com>
2018-06-28 15:33:27 -0400

    added double genre to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@ Artist.destroy_all
 Genre.destroy_all
 SongGenre.destroy_all
 LibraryParser.parse
+
+luis_fonsi = Artist.create(name: "Luis Fonsi")
+despacito = Song.create(name: "Despacito")
+luis_fonsi.songs << despacito
+Genre.find_by(name: 'pop').songs << despacito
+Genre.find_by(name: 'dance').songs << despacito
